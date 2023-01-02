25138edb717d155a88c011616327aa7b269663e3
Mohsen Saberi <m.saberi1999@gmail.com>
2023-01-02 10:39:47 +0100

    add MigrationDatabase Method to Program.cs and ContextSeeder alos add Index partial views

--- PersonalWebsite_v2/Program.cs
+++ PersonalWebsite_v2/Program.cs
@@ -1,32 +1,56 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using PersonalWebsite_v2.Extentions;
 using PersonalWebsite_v2.Models;
 using PersonalWebsite_v2.Repository;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDefaultIdentity<User>().AddRoles<IdentityRole>().AddEntityFrameworkStores<RepositoryContext>();
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace test
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+			//builder.Services.AddDefaultIdentity<User>().AddRoles<IdentityRole>().AddEntityFrameworkStores<RepositoryContext>();
+			//var host = CreateHostBuilder(args).Build();
+			//host.MigrateDatabase<RepositoryContext>((context, services) =>
+			//{
+			//	var logger = services.GetService<ILogger<RepositoryContextSeed>>();
+			//	RepositoryContextSeed.SeedAsync(context, logger).Wait();
+
+			//});
+			//host.Run();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+				Host.CreateDefaultBuilder(args)
+						.ConfigureWebHostDefaults(webBuilder =>
+						{
+						});
+	}
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-		name: "default",
-		pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
---
+++ PersonalWebsite_v2/Repository/RepositoryContextSeed.cs
@@ -0,0 +1,65 @@
+﻿using PersonalWebsite_v2.Models;
+
+namespace PersonalWebsite_v2.Repository
+{
+	public class RepositoryContextSeed
+	{
+		public static async Task SeedAsync(RepositoryContext context, ILogger<RepositoryContextSeed> logger)
+		{
+			if (!context.PersonalInfo.Any())
+			{
+				context.PersonalInfo.Add(GetPreconfiguredPersonalInfo());
+				await context.SaveChangesAsync();
+				logger.LogInformation("Seed database associated with context {DbContextName}", typeof(RepositoryContext).Name);
+			}
+			if (!context.Skills.Any())
+			{
+				context.Skills.AddRange(GetPreconfiguredSkills());
+				await context.SaveChangesAsync();
+				logger.LogInformation("Seed database associated with context {DbContextName}", typeof(RepositoryContext).Name);
+			}
+			if (!context.Fact.Any())
+			{
+				context.Fact.Add(GetPreconfiguredFact());
+				await context.SaveChangesAsync();
+				logger.LogInformation("Seed database associated with context {DbContextName}", typeof(RepositoryContext).Name);
+			}
+		}
+
+		private static PersonalInfo GetPreconfiguredPersonalInfo()
+		{
+			return new PersonalInfo
+						{
+								Id = Guid.NewGuid(),
+								AboutMe = "My name is Mohsen Saberi",
+								Age = "24",
+								BirthDay = "12 November 1998",
+								CityOfResidence = "Iran, Mashhad",
+								Degree = "Bachelors in Mechanical Engineering",
+								Email = "M.saberi1999@gmail.com",
+								Name = "Mohsen Saberi"
+			};
+		}
+
+		private static IEnumerable<Skill> GetPreconfiguredSkills()
+		{
+			return new List<Skill>
+			{
+				new Skill() {Id = Guid.NewGuid() , Name = "Asp.NET" , Percent = 90},
+				new Skill() {Id = Guid.NewGuid(), Name = "Csharp", Percent = 90}
+			};
+		}
+
+		private static Fact GetPreconfiguredFact()
+		{
+			return new Fact
+			{
+				Id = Guid.NewGuid(),
+				Awards = 5,
+				HappyClient = 10,
+				HoursOfSupport = 1800,
+				Projects = 19
+			};
+		}
+	}
+}
